f92df9425914b4f503e875d8fdc8686db96c8779
khaledGuedria <khaled.guedria.1@esprit.tn>
2021-05-09 07:11:58 +0100

    Initial Commit

---
+++ QuranApp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  QuranApp
+//
+//  Created by Khaled Guedria on 5/9/21.
+//  Copyright © 2021 Khaled Guedria. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
